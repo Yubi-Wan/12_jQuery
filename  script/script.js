864b2347c04d05dc5ca43a10e2e273017f21f421
ユビワンYubi-Wan <scph50000nb@gmail.com>
2020-12-19 23:43:08 +0900

    更新

---
+++  script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    alert("文字を出します")
+});
+
+$(document).ready(function(){
+    $(".hoge").css("background-color","blue")
+});
+
+$(document).ready(function(){
+    $(".hoge").html("<div class="fugafuga">htmlを変えたぜ!</div>")
+});
